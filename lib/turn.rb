0b274391b9ca4dcd495a0b947b1f5dfbda4626e0
Andrew Bay <daddnbay@yahoo.com>
2019-03-12 21:31:04 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -14,6 +14,7 @@ def input_to_index(user_input)
   return index
 end
 
+
 def valid_move?(board, index)
   def position_taken?(array, ind)
     if array[ind] == " " || array[ind] == "" || array[ind] == nil
