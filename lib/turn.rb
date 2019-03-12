7c13be41b30fedc6de281d025ac815f707dc4560
Andrew Bay <daddnbay@yahoo.com>
2019-03-12 21:30:00 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -7,6 +7,12 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
+#coverts a user's place on the board to the index integer
+def input_to_index(user_input)
+  index = user_input.to_i
+  index -= 1
+  return index
+end
 
 def valid_move?(board, index)
   def position_taken?(array, ind)
@@ -33,7 +39,5 @@ def valid_move?(board, index)
 end
 
 def turn(board)
-  puts "Please enter 1-9:"
-end
-
-def input _to index()
+  puts ""
+  
